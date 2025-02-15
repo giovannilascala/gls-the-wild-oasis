99e6f13f494e74710d5d3caa3614c73703cce145
giovannilascala <giovanni.lascala.07.gls@gmail.com>
2025-02-15 21:48:18 +0100

    Readme modified

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@ui': path.resolve(__dirname, 'src/ui'),  // Aggiungi un alias per 'ui'
+    },
+  },
 });
+
